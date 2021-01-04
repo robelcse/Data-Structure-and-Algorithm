5aaf4277b3c09b38f2854da3c0e056891ed38ea7
Md.Robel Ahmed <76914100+robelcse@users.noreply.github.com>
2021-01-04 16:19:11 +0600

    linear search

---
+++ 1.linear-search/linear_search.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+/*
+ Linear search
+*/
+
+int main(){
+
+  int n,  i, s_data;
+  bool found = true;
+
+  cout<<"Please enter the size of array:"<<endl;
+  cin>>n;
+  int arr[n];
+  cout<<"Please enter the elements of array:"<<endl;
+  for(i = 0; i < n ; i++){
+       cin>>arr[i];
+  }
+
+  cout<<"please enter the data which you want to search:"<<endl;
+  cin>>s_data;
+
+ for(i = 0; i < n ; i++){
+        if(arr[i] == s_data){
+              cout<<"the index is : "<<i<<" "<<"and data is :"<<arr[i]<<endl;
+              found = false;
+              break;
+        }
+  }
+
+  if(found){
+       cout<<"Data not found!";
+  }
+
+  return 0;
+}
