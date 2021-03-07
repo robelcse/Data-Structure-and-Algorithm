bf4454ec35f95edc2564212429784874ae290789
Md.Robel Ahmed <76914100+robelcse@users.noreply.github.com>
2021-03-07 21:18:18 +0600

    divisor

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,8 +1,38 @@
 #include<iostream>
 using namespace std;
 
+bool marked[30];
+void sieve(int n) {
+  for (int i = 2; i < n; i++) {
+    if (marked[i] == false) { // i is a prime
+      for (int j = i + i; j <= n; j += i) {
+        marked[j] = true;
+      }
+    }
+  }
+
+
+}
+
+bool isPrime(int n) {
+  if (n < 2) return false;
+  return marked[n] == false;
+}
+
 int main(){
 
-    cout<<"Hello world";
-    return 0;
+   int n = 30;
+   sieve(n);
+   for(int i = 0; i <n; i++){
+          cout<<marked[i]<<" ";
+    }
+
+    cout<<endl;
+
+
+    bool result = isPrime(11);
+    cout<<result;
+
+
+     return 0;
 }
